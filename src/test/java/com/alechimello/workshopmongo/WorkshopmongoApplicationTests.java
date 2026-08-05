8969d9e8b767cf2d14cc0d9e72d8d9197b6ec2d3
Alexandre Felli Chimello <aleirado@outlook.com>
2026-08-05 18:06:23 -0300

    Project mongoDB NOSQL created

---
+++ src/test/java/com/alechimello/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alechimello.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
